9b02e36c7b35f4998ab118a26fc3e0f5eaa31a2e
hschuelter <arthur.schuelter@gmail.com>
2025-03-19 13:55:05 -0300

    :sparkles:feat: fix header link

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
 		<>
 			<div className="flex flex-col bg-black">
 				<Label className="text-white font-mono text-2xl m-4 ml-6 self-start"> 
-					<a className="text-white hover:text-white" href="http://localhost:5173/">
+					<a className="text-white hover:text-white" href="https://encurta-ai.vercel.app/">
 						encurta.ai 
 					</a>
 				</Label>
